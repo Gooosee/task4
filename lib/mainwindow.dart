a668f800bcfb8f14a274aeb5420ca07b99c3ed19
Big_Goose <tima123azaza@gmail.com>
2021-12-20 14:00:23 +0300

    Tab bar

---
+++ lib/mainwindow.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+import 'screens/tab1.dart';
+import 'screens/tab2.dart';
+import 'screens/tab3.dart';
+import 'screens/tab4.dart';
+import 'screens/tab5.dart';
+
+class MainWindow extends StatefulWidget {
+  const MainWindow({Key? key}) : super(key: key);
+
+  @override
+  _MainWindowState createState() => _MainWindowState();
+}
+
+class _MainWindowState extends State<MainWindow> {
+  int tabIndex = 0;
+  late List<Widget> listScreens;
+  @override
+  void initState() {
+    super.initState();
+    listScreens = [
+      Tab1(),
+      Tab2(),
+      Tab3(),
+      Tab4(),
+      Tab5(),
+    ];
+  }
+
+//  @override
+//  bool get wantKeepAlive =>
+//      true; //by default it will be null, change it to true.
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      color: Colors.yellow,
+      home: Scaffold(
+        body: listScreens[tabIndex],
+        bottomNavigationBar: BottomNavigationBar(
+            selectedItemColor: Colors.black,
+            unselectedItemColor: Colors.grey[400],
+            backgroundColor: Theme.of(context).primaryColor,
+            currentIndex: tabIndex,
+            onTap: (int index) {
+              setState(() {
+                tabIndex = index;
+              });
+            },
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                title: Text('Tab1'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.report_problem),
+                title: Text('Tab2'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                title: Text('Tab3'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                title: Text('Tab4'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                title: Text('Tab5'),
+              ),
+            ]),
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+    );
+  }
+}
---
+++ lib/screens/tab5.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Tab5 extends StatefulWidget {
+  @override
+  _Tab5State createState() => _Tab5State();
+}
+
+class _Tab5State extends State<Tab5> with AutomaticKeepAliveClientMixin<Tab5> {
+  @override
+  void initState() {
+    super.initState();
+    print('initState Tab5');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('build Tab5');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tab5'),
+      ),
+      body: Center(
+        child: Text(
+          'This is content of Tab5',
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+    );
+  }
+
+  @override
+  bool get wantKeepAlive => true;
+}
